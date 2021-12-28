8815056f5c29df271cc28ea6b7914ef56d3d7c57
Steve Korshakov <steve@korshakov.com>
2021-12-28 22:32:52 +0300

    feat: upgrade test executor

--- src/pool-stakes.spec.ts
+++ src/pool-stakes.spec.ts
@@ -1,15 +1,15 @@
 import fs from 'fs';
-import { Address, CommentMessage, InternalMessage, toNano } from 'ton';
+import { Address, Cell, CommentMessage, InternalMessage, toNano } from 'ton';
 import { SmartContract } from "ton-contract-executor";
 import { createPoolData } from './pool-data';
 import { createTestAddress } from './utils/createTestAddress';
 
-const source = fs.readFileSync(__dirname + '/contract/main-amalgama.fc', 'utf-8');
+const source = Cell.fromBoc(fs.readFileSync(__dirname + '/contract/main.cell'))[0];
 const owner = createTestAddress(0, 'owner');
 
 describe('pool-stakes', () => {
     it('should accept stakes', async () => {
-        const contract = await SmartContract.fromFuncSource(source, createPoolData(owner, 0));
+        const contract = await SmartContract.fromCell(source, createPoolData(owner, 0));
         let res = await contract.sendInternalMessage(new InternalMessage({
             to: owner,
             value: toNano(1),
